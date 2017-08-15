5b4948d2c107713492090d1239d4a255eda0d7f8
Kristen Kulha and snow vilay <kristen@>
2017-08-15 11:47:03 -0700

    Add functionality to form and add warning for when over 4 options are selected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-
+  $("#stress-form").submit(function(event){
+    event.preventDefault();
+    var surveyTotal = 0;
+    $("#experience").show();
+    $("input:checkbox[name=exp]:checked").each(function() {
+      var userExp = $(this).val();
+      $("#experience").append(userExp + "<br>");
+      surveyTotal ++;
+    });
+    $("#health").show();
+    $("input:checkbox[name=health]:checked").each(function() {
+      var userHealth = $(this).val();
+      $("#health").append(userHealth + "<br>");
+      surveyTotal ++;
+    });
+    if (surveyTotal >= 4) {
+      $("#warning").show();
+    }
+    $("#stress-form").hide();
+  });
 });
